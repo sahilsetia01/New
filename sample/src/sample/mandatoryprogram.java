b6b25eb343e485c68f68b9e3ff6e4c36ad0794b9
Sahil Setia <sahilsetia.setia@gmail.com>
2019-05-02 16:05:22 +0530

    initial commit

---
+++ sample/src/sample/mandatoryprogram.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class mandatoryprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The first mandatory program");
+
+	}
+
+}
